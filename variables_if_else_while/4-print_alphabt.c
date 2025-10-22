6c6edf9593ededd5a7153e24bbfe1f24577c8930
dnkrow <malouvaldes@gmail.com>
2025-10-22 08:50:44 +0000

    feat:Print all the letters except q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Write program prints the alphabet in lowercase followed by a new line
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
