4272aafae335a06a03fd370efa6396e8cb73217d
dnkrow <malouvaldes@gmail.com>
2025-10-29 09:32:26 +0000

    fix: function that convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,3 +7,18 @@
  * Return: converted integer
  */
 int _atoi(char *s)
+{
+	int i = 0, sign = 1, res = 0;
+	int seen_digit = 0;
+
+	while (s[i] != '\0' && !seen_digit)
+	{
+		if (s[i] == '-')
+			sign = -sign;
+		else if (s[i] >= '0' && s[i] <= '9')
+			seen_digit = 1;
+
+		if (!seen_digit)
+			i++;
+	}
+}
